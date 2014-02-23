89c4dc0473d43817ec547c49f1b469cf6ac423c9
smspezzano <smspezzano@gmail.com>
2014-02-23 15:31:47 -0800

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 IdealMe::Application.routes.draw do
 
-  get "activities/new"
-  get "activities/create"
-  get "activities/show"
-  get "activities/edit"
-  get "site/index"
   root :to => "site#index"
+
+  resources :users do
+    resources :idealchart
+  end
+
+  resources :users do
+    resources :realchart
+  end
+
+  resources :realchart do 
+    resources :activities
+  end
 
   devise_for :users
 
