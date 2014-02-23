b45596836e19787e7510b65216f4694fbcc87c1f
Isa Herico <miss.herico@gmail.com>
2014-02-23 14:46:59 -0800

    added activities controller

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,13 @@
+class ActivitiesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 IdealMe::Application.routes.draw do
 
+  get "activities/new"
+  get "activities/create"
+  get "activities/show"
+  get "activities/edit"
   get "site/index"
   root :to => "site#index"
 
