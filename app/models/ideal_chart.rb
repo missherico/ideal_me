454a5f7909ba67ef1af12e9fdaad4991307c271e
Isa Herico <miss.herico@gmail.com>
2014-02-23 15:01:45 -0800

    created seed file to finalize category names

--- app/models/ideal_chart.rb
+++ app/models/ideal_chart.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: ideal_charts
+#
+#  id              :integer          not null, primary key
+#  user_id         :integer
+#  health_score    :integer
+#  social_score    :integer
+#  intellect_score :integer
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+
 class IdealChart < ActiveRecord::Base
   belongs_to :user
 
