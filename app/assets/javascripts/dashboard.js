ec0a2e7f2bf2d08704668f930519be47906b7b02
smspezzano <smspezzano@gmail.com>
2014-02-25 12:29:21 -0800

    implemented js in dashboard.js to update with the form

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -15,14 +15,44 @@ window.onload = function () {
 
 
 
-  IdealData.updateData = function(){
+  IdealData.updateIdealData = function(data, callback){
     $.ajax ({ 
-      url : this.urls.update.path + gon.user_id + 'ideal_charts/' + gon.idealchart.id,
+      url : this.urls.update.path + gon.user_id + '/ideal_charts/' + gon.idealchart.id+".json",
       type : this.urls.update.method,
-      data : data 
-    });
+      data : {ideal_chart: data} 
+    }).done(callback);
   };
 
+  IdealData.doThis = function(fn){
+    fn.apply(IdealData);
+    return this;
+  };
+
+
+  // event handler for updating idealchart
+  IdealData.doThis(function(){
+    var _this = this;
+
+    $("#updateIdeal").on("submit", function(event){
+      event.preventDefault();
+
+      var updatedIdealData = {};
+      updatedIdealData.health_score = $("#health").val();
+      updatedIdealData.social_score = $("#social").val();
+      updatedIdealData.intellect_score = $('#intellect').val();
+      _this.updateIdealData(updatedIdealData, function(data){
+        var chartData = chart.options.data[0].dataPoints;
+        var newIdealData = data.ideal_chart;
+        console.log(newIdealData);
+        chartData[0].y = newIdealData.health_score;
+        chartData[1].y  = newIdealData.social_score;
+        chartData[2].y  = newIdealData.intellect_score;
+        chart.render();
+      });
+
+
+    });    
+  });
 
   var chart = new CanvasJS.Chart("chartContainer",
     {
