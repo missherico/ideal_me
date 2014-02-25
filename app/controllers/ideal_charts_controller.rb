255661586cce0bf066783d1e265ebeeafd305960
smspezzano <smspezzano@gmail.com>
2014-02-24 20:03:21 -0800

    took out binding.pry

--- app/controllers/ideal_charts_controller.rb
+++ app/controllers/ideal_charts_controller.rb
@@ -15,7 +15,6 @@ class IdealChartsController < ApplicationController
     @idealchart = IdealChart.new(new_idealchart)
     @idealchart.user_id = new_user_id
     @idealchart.save
-    #binding.pry
     respond_to do |format|
       format.html {redirect_to user_dashboard_path(current_user.id)}
       format.json {render json: @idealchart}
