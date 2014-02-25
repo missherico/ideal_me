e3feaa17b098bf4829378422a7146de4ac09980a
smspezzano <smspezzano@gmail.com>
2014-02-25 10:02:34 -0800

    added urls to get IdealChart data and to update IdealChart

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,10 +1,20 @@
 window.onload = function () {
 
   
-  // instantiate 
+  // instantiate IdealData
+  // this will hold all of the functions and data needed to create a chart
   var IdealData = {};
 
   
+  // Creating the urls hash that will be referred to when making ajax requests 
+  IdealData.urls = {
+    // url to get the IdealChart data
+    // /users/:user_id/ideal_charts/:id
+    show : { path : '/users/', method : 'get'},
+    // url to update the IdealChart data
+    // /users/:user_id/ideal_charts/:id
+    update : { path : '/users/', method: 'patch'}
+  };
 
 
 
