ee672a1fb5a0cdeb0f276637008f96613eb1521c
Isa Herico <miss.herico@gmail.com>
2014-02-24 19:12:52 -0800

    resolving

--- db/migrate/20140224221954_edit_real_chart_id_column_to_activities.rb
+++ db/migrate/20140224221954_edit_real_chart_id_column_to_activities.rb
@@ -1,5 +1,6 @@
 class EditRealChartIdColumnToActivities < ActiveRecord::Migration
   def change
     rename_column :activities, :realchart_id, :real_chart_id
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140224223606) do
+ActiveRecord::Schema.define(version: 20140224221954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
