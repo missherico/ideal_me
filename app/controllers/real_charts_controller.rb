85303c7effcdb9fc9d5338019ff4145da8f0186e
SylvieC <sylviecottrell@gmail.com>
2014-02-24 17:54:40 -0800

    took out the binding.pry

--- app/controllers/real_charts_controller.rb
+++ app/controllers/real_charts_controller.rb
@@ -1,5 +1,19 @@
 class RealChartsController < ApplicationController
+   before_filter :authenticate_user!
 
+   def index
+    realcharts = RealChart.all
+   end
+
+   def create
+    new_realchart = RealChart.create(user_id: current_user.id)
+   
+   end
+
+   def new
+    @realchart = RealChart.new
+   end
+  
 	
 
 
