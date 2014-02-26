d972a2a22df48f9cf28e066af64ef010c418cd7d
SylvieC <sylviecottrell@gmail.com>
2014-02-26 14:55:18 -0800

    added Sidetiq gem in Gemfile

--- app/workers/email_worker.rb
+++ app/workers/email_worker.rb
@@ -1,6 +1,23 @@
 class EmailWorker
-  include Sidekiq::EmailWorker
+  include Sidekiq::Worker
+  include Sidetiq::Schedulable
 
-   def perform(current_user.id)
+  recurrence { daily }
+
+   def perform(userPId)
+     @users = User.all
+     @users.each do |user|
+      @user.activities do |user_activity|
+        time_since_last_activity = Time.now - user_activity.created_at
+        #A day has 86,400 seconds, 5 days has 432,000 seconds
+        if (time_since_last_activity > 432,000)
+          RemdinderMailer.remind(user).deliver
+        end
+     end
    end
+
  end
+
+    
+          
+   end
