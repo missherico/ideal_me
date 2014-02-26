0aee46e6ec3060e22d5a599b513621d38d953b7d
elidarrow <elidarrow@gmail.com>
2014-02-26 15:07:39 -0800

    set aws keys and bucket name to env, included carrierwave mimetypes in image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -10,6 +10,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # storage :file
   storage :fog
 
+  include CarrierWave::MimeTypes
+  process :set_content_type
+
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
