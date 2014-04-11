3a86877d6d0fd3348cbfbc72513dccb4204383d0
smspezzano <smspezzano@gmail.com>
2014-04-11 11:21:21 -0700

    WIP ideal_chart spec: update attribute test passing

--- spec/controllers/ideal_charts_controller_spec.rb
+++ spec/controllers/ideal_charts_controller_spec.rb
@@ -10,6 +10,13 @@ describe IdealChartsController do
 	  		@user = FactoryGirl.create(:user)
 	  		sign_in @user
 
+	  		@new_ideal_chart = IdealChart.create(
+	  			user_id: @user.id, 
+	  			health_score: 20,
+	  			social_score: 30,
+	  			intellect_score: 50
+	  		)
+
 	  	end
 
 	  	# it "should assign @real_chart" do
@@ -24,13 +31,19 @@ describe IdealChartsController do
 	  	# end
 
 	  	it "should create RealChart with the creation of IdealChart" do
-	  		new_ideal_chart = IdealChart.create(
-	  			user_id: @user.id, 
-	  			health_score: 20,
-	  			social_score: 30,
-	  			intellect_score: 50
-	  		 )
 	  		expect(RealChart.find_by(user_id: @user.id)).should_not eql(nil)
+	  	end
+
+	  	it "should update attributes" do
+	  		@new_ideal_chart.update_attributes(
+	  			health_score: 30,
+	  			social_score: 50,
+	  			intellect_score: 20
+	  		)
+
+	  		expect(@new_ideal_chart.health_score).to be == 30
+	  		expect(@new_ideal_chart.social_score).to be == 50
+	  		expect(@new_ideal_chart.intellect_score).to be == 20
 	  	end
 	  end
 
